762db00c6d5cef90fe943ea37818bd56c091e08f
michael stepniak <michaelstepniak@michaels-MBP.attlocal.net>
2024-09-17 16:31:49 -0400

    sandbox

--- CS50-Python/adventure/sandbox.py
+++ CS50-Python/adventure/sandbox.py
@@ -9,6 +9,13 @@ class Player:
             inventory[item] = 1
         else:
             inventory[item] += 1
+
+    def view_inventory(self, inventory):
+        ...
+
+    def remove_item(self, item, inventory):
+        ...
+
         
         
 obtainable_items = ["sword", "magic boots", "apple", "healing potion"]
@@ -21,21 +28,6 @@ def main():
     print(inventory)
     player.pick_up_item(obtainable_items, inventory)
     print(inventory)
-    """
-    add_item_to_inventory()
-    print(inventory)
-    add_item_to_inventory()
-    print(inventory)
-    """
-    
-"""
-def add_item_to_inventory():
-    item = obtainable_items[0]
-    if not item in inventory:
-        inventory[item] = 1
-    else:
-        inventory[item] += 1
-"""
 
 
 
